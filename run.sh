6f3c0f3acacec405ac07ea06e2da7b01444b0f17
rhit-chenz20 <chenz20@rose-hulman.edu>
2022-09-09 22:26:02 -0400

    cleaned up csv files

--- run.sh
+++ run.sh
@@ -8,17 +8,16 @@ mkdir result/${date}/CSV
 mkdir result/${date}/plot
 mkdir result/${date}/CSV/exp_4
 
-for SIZE in 10
+for SIZE in 100
 do
-    for N in 2 4 6
+    for N in 4 6
     do
-    for V in {1..10}
+    for V in 1
     do
         let "count+=1"
         echo "running $count"
 
-        python3 run.py -size $SIZE -length 15 -mutateR 0.1 -points 15 -prange 10 -fn "result/${date}/CSV/exp_4/${V}_" -e 0 -sel 1 -fit 0 -max 1 -c 0 -n $N
-        # -seed 7431
+        python3 run.py -size $SIZE -length 15 -mutateR 0.1 -points 15 -prange 10 -fn "result/${date}/CSV/exp_4/${V}_${N}_" -e 0 -sel 1 -fit 0 -max 1000 -c 1 -n $N -seed 47491
     done
     done
 done
