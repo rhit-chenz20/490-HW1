f4364789fd03eab720b0d0819e8570fc4d649b99
rhit-deckerza <deckerza@rose-hulman.edu>
2022-09-23 20:44:48 -0400

    awe

--- hw3_EoC/run.sh
+++ hw3_EoC/run.sh
@@ -5,21 +5,58 @@ declare max=30
 mkdir result
 mkdir result/${date}
 
-for WIDTH in 100
+for WIDTH in 128
 do
     for V in 1
     do
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334 
-        python run.py -width $WIDTH  -duration 1000 -rule 1 -fn "result/${date}/" -l .334
+        for L in .65
+        do
+            for D in 1000
+            do
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L 
+                python run.py -width $WIDTH  -duration $D -rule 1 -fn "result/${date}/" -l $L
+            done
+        done
     done
 done
-
 echo "Finished simulation."
